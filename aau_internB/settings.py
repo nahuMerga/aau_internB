f7a454e089ee3dc66101a2ec77ab9a0eeb12378e
NahoMer <143824687+nahuMerga@users.noreply.github.com>
2025-04-04 11:14:26 -0300

    Update settings.py

--- aau_internB/settings.py
+++ aau_internB/settings.py
@@ -164,7 +164,7 @@ USE_TZ = True
 
 # Static Files
 STATIC_URL = "/static/"
-STATICFILES_DIRS = os.path.join(BASE_DIR, "static")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
