2f5036e840cc03ac4515b588e2ba74c1e7346658
NahoMer <143824687+nahuMerga@users.noreply.github.com>
2025-04-04 10:44:03 -0300

    Update wsgi.py

--- aau_internB/wsgi.py
+++ aau_internB/wsgi.py
@@ -14,6 +14,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aau_internB.settings')
 
 application = get_wsgi_application()
-
-# Use vercel_wsgi handle
-app = application
