fa24b836fe496f93a72ae67b003a62d837ea4925
NahoMer <143824687+nahuMerga@users.noreply.github.com>
2025-04-07 14:18:14 -0300

    Update signals.py

--- students/signals.py
+++ students/signals.py
@@ -1,12 +1,1 @@
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from students.models import Student
 
-@receiver(post_save, sender=Student)
-def advisor_assignment_changed(sender, instance, **kwargs):
-    """
-    Triggered whenever a Student object is saved.
-    If 'assigned_advisor' changes, perform an action.
-    """
-    if instance.assigned_advisor:  # Check if advisor was assigned
-        print("Hello, World! Advisor status changed.")
