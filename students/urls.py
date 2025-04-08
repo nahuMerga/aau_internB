bdf9e38adfbd3cde50c11e824cb7706adaf4ce50
NahoMer <143824687+nahuMerga@users.noreply.github.com>
2025-04-08 09:37:09 -0300

    Update urls.py

--- students/urls.py
+++ students/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from .views import  InternshipOfferLetterUploadView, InternshipReportUploadView, StudentRegistrationView
+from .views import (
+    StudentRegistrationView,
+    InternshipOfferLetterUploadView,
+    InternshipReportUploadView,
+    OfferLetterStatusView,
+    ReportStatusView
+)
 
 urlpatterns = [
     path("register/", StudentRegistrationView.as_view(), name="student-register"),
-    path('upload-offer-letter/', InternshipOfferLetterUploadView.as_view(), name='upload-offer-letter'),
-    path('upload-report/', InternshipReportUploadView.as_view(), name='upload-report'),
+    path("upload-offer-letter/", InternshipOfferLetterUploadView.as_view(), name="upload-offer-letter"),
+    path("upload-report/", InternshipReportUploadView.as_view(), name="upload-report"),
+    path("offer-letter-status/", OfferLetterStatusView.as_view(), name="offer-letter-status"),
+    path("report-status/", ReportStatusView.as_view(), name="report-status"),
 ]
