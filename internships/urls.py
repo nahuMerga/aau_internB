7aa1630a5f57fb06a6984f24c7dea5734dfad169
NahoMer <143824687+nahuMerga@users.noreply.github.com>
2025-04-17 13:46:45 -0300

    Update urls.py

--- internships/urls.py
+++ internships/urls.py
@@ -4,12 +4,14 @@ from .views import (
     AdminAdvisorsListView,
     AssignAdvisorView,
     InternshipPeriodView,
-    AutoAssignAdvisorsView
+    AutoAssignAdvisorsView,
+    CompanyListCreateView
 )
 
 
 urlpatterns = [
     path('students/', AdminStudentsListView.as_view(), name='admin-students-list'),
+    path('companies/', CompanyListCreateView.as_view(), name='company-list-create'),
     path('advisors/', AdminAdvisorsListView.as_view(), name='admin-advisors-list'),
     path('assign-advisor/', AssignAdvisorView.as_view(), name='assign-advisor'),
     path('internship-period/', InternshipPeriodView.as_view(), name='internship-period'),
