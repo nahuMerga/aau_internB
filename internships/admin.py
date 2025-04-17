dbf8c93035fd6f86c79c33eca41a6de67627d3df
NahoMer <143824687+nahuMerga@users.noreply.github.com>
2025-04-17 13:45:02 -0300

    Update admin.py

--- internships/admin.py
+++ internships/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
-from .models import ThirdYearStudentList, InternStudentList, InternshipPeriod
+
+from .models import ThirdYearStudentList, InternStudentList, InternshipPeriod, Company, Internship ,InternshipHistory
 
 admin.site.register(ThirdYearStudentList)
-
+admin.site.register(Company)
 admin.site.register(InternStudentList)
-
+admin.site.register(InternshipHistory)
+admin.site.register(Internship)
 admin.site.register(InternshipPeriod)
